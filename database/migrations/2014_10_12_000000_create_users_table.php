436f2202368e4ba803fc1280b6985975b1d59167
Achraf Bchir <achraf.achraf.ba@gmail.com>
2021-02-14 10:24:46 +0100

    create project

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('email')->unique();
+            $table->boolean('is_email_verified')->default(false);
+            $table->text('activation_token')->nullable();
+            //$table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->string('phone')->unique();
+            $table->boolean('is_phone_verified')->default(false);
+            $table->string('first_name')->nullable();
+            $table->string('last_name')->nullable();
+            $table->string('profil_picture')->nullable();
+            $table->text('actuality')->nullable();
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
